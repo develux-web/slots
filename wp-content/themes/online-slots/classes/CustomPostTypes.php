6d2e6a03ec48ad0cf64101e0cd4c3dba70a4d71f
StebDev <steblyuk.kostya@gmail.com>
2023-05-11 13:04:24 +0300

    theme settings

--- wp-content/themes/online-slots/classes/CustomPostTypes.php
+++ wp-content/themes/online-slots/classes/CustomPostTypes.php
@@ -7,6 +7,8 @@ class CustomPostTypes
     public function __construct(){
         add_action( 'init', [$this, 'slots'] );
         add_action( 'init', [$this, 'casino'] );
+        add_action( 'init', [$this, 'features'] );
+        add_action( 'init', [$this, 'themesSlots'] );
     }
 
     public function slots(): void{
@@ -67,6 +69,70 @@ class CustomPostTypes
             'menu_position'      => null,
             'supports'           => array('title','thumbnail','page-attributes')
         ) );
+    }
+
+    public function features(): void
+    {
+        register_taxonomy('features', 'slots', [
+            'label'        => '',
+            'labels'       => [
+                'name'              => 'Features',
+                'singular_name'     => 'Features',
+                'search_items'      => 'Search features',
+                'all_items'         => 'All features',
+                'view_item '        => 'View features',
+                'parent_item'       => 'Parent features',
+                'parent_item_colon' => 'Parent features:',
+                'edit_item'         => 'Edit features',
+                'update_item'       => 'Update features',
+                'add_new_item'      => 'Add New features',
+                'new_item_name'     => 'New features Name',
+                'menu_name'         => 'Features',
+                'back_to_items'     => '← Back to Features',
+            ],
+            'description'  => '',
+            'public'       => true,
+            'hierarchical' => true,
+
+            'rewrite'           => true,
+            'capabilities'      => array(),
+            'meta_box_cb'       => null,
+            'show_admin_column' => false,
+            'show_in_rest'      => null,
+            'rest_base'         => null,
+        ]);
+    }
+
+    public function themesSlots(): void
+    {
+        register_taxonomy('themes', 'slots', [
+            'label'        => '',
+            'labels'       => [
+                'name'              => 'Themes',
+                'singular_name'     => 'Themes',
+                'search_items'      => 'Search themes',
+                'all_items'         => 'All themes',
+                'view_item '        => 'View themes',
+                'parent_item'       => 'Parent themes',
+                'parent_item_colon' => 'Parent themes:',
+                'edit_item'         => 'Edit themes',
+                'update_item'       => 'Update themes',
+                'add_new_item'      => 'Add New themes',
+                'new_item_name'     => 'New themes Name',
+                'menu_name'         => 'Themes',
+                'back_to_items'     => '← Back to Themes',
+            ],
+            'description'  => '',
+            'public'       => true,
+            'hierarchical' => true,
+
+            'rewrite'           => true,
+            'capabilities'      => array(),
+            'meta_box_cb'       => null,
+            'show_admin_column' => false,
+            'show_in_rest'      => null,
+            'rest_base'         => null,
+        ]);
     }
 }
 
--- wp-content/themes/online-slots/functions.php
+++ wp-content/themes/online-slots/functions.php
@@ -3,14 +3,4 @@ include_once 'classes/ThemeConfiguration.php';
 include_once 'classes/CustomPostTypes.php';
 
 
-if( function_exists('acf_add_options_page') ) {
 
-    acf_add_options_page(array(
-        'page_title'    => '404 page',
-        'menu_title'    => 'Theme Settings',
-        'menu_slug'     => 'theme-general-settings',
-        'capability'    => 'edit_posts',
-        'redirect'      => false
-    ));
-
-}
